b4cc03cd8b4cdd0e864eed8676a31e7e24d99a31
Adedotun-Adepoju <d.e.adepoju@gmail.com>
2023-11-24 19:09:29 +0100

    implement-chatgpt-chatbot

--- chatbot/src/app/api/chat/route.ts
+++ chatbot/src/app/api/chat/route.ts
@@ -1,44 +1,72 @@
 import { ReplicateStream, StreamingTextResponse } from 'ai';
-import Replicate from 'replicate';
-import { experimental_buildLlama2Prompt } from 'ai/prompts';
+// import axios from 'axios';
+
+// const axiosInstance = axios.create();
+
  
-// Create a Replicate API client (that's edge friendly!)
-const replicate = new Replicate({
-  auth: process.env.REPLICATE_API_KEY || '',
-});
- 
-// IMPORTANT! Set the runtime to edge
+const apiKey = "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"
+
+const apiUrl = "https://api.openai.com/v1/chat/completions"
+
+const systemPrompt = "You are an AI assistant that diagnoses the symptoms given by a user and asks questions one after the other to understand more about the symptom before arriving at the disease. Please ask just 3 follow up questions one by one so the user is not overwhelmed. After diagnosing the disease, I want you to ask the user if they want suggested treatments or they want to visit the nearest hospital."
+
+const requestData = {
+  "model": "gpt-3.5-turbo",
+ "messages": [
+     {
+         "role": "system",
+         "content": systemPrompt
+     },
+ ],
+ "temperature": 0.5,
+ "max_tokens": 256
+}
+
+
 export const runtime = 'edge';
  
 export async function POST(req: Request) {
   console.log("here")
   const { messages } = await req.json();
-  console.log(messages)
+
+  requestData.messages.push(messages[messages.length - 1])
+
+  console.log("message", messages)
+  console.log("request data", requestData)
+
+  const response = await fetch(apiUrl, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      'Authorization': `Bearer ${apiKey}`,
+    },
+    body: JSON.stringify(requestData)
+  })
+
+  const responseData = await response.json()
+  // console.log(responseData)
+  console.log("respomse data", responseData.choices[0].message.content)
  
-  const response = await replicate.predictions.create({
-    // You must enable streaming.
-    stream: true,
-    // The model must support streaming. See https://replicate.com/docs/streaming
-    // This is the model ID for Llama 2 70b Chat
-    version: '2c1608e18606fad2812020dc541930f2d0495ce32eee50074220b87300bc16e1',
-    // Format the message list into the format expected by Llama 2
-    // @see https://github.com/vercel/ai/blob/99cf16edf0a09405d15d3867f997c96a8da869c6/packages/core/prompts/huggingface.ts#L53C1-L78C2
-    input: {
-      prompt: experimental_buildLlama2Prompt(messages),
-      system_prompt: "You are an AI that diagnoses the symptoms given by a user and asks questions in steps to understand more about the symptom before arriving at the disease. Please ask the follow up questions in a conversational manner one at a time in a way they can produce their answer in Yes/No/I don't know. After diagnosing the disease, I want you to ask the user if they want suggested treatments or they want to visit the nearest hospital."
+  // Create a new ReadableStream
+  const readableStream = new ReadableStream({
+    start(controller) {
+      // This is where you handle the data and push it into the stream
+      // For example, assuming response.data contains the streaming data
+      controller.enqueue(responseData.choices[0].message.content);
     },
   });
- 
-  // Convert the response into a friendly text-stream
-  const stream = await ReplicateStream(response, {
-    onStart: async () => {
-      console.log("start")
-    },
-    onCompletion: async (completion: string) => {
-      console.log("completion", completion)
-    }
+
+  const aiResponse = responseData.choices[0].message.content
+
+  requestData.messages.push({
+    "role": "assistant",
+    "content": aiResponse
   })
-  // const stream = await ReplicateStream(response);
-  // Respond with the stream
-  return new StreamingTextResponse(stream);
+
+  console.log("finally", requestData)
+
+  // Create a new Response object with the ReadableStream
+  // const newResponse = new Response(readableStream);
+
+  return new StreamingTextResponse(readableStream);
 }
