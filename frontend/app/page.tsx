4b0c2a9348b4b74a88c38492f74deeffb762d202
nonfungiblehayor <nonfungiblehayor.eth@gmail.com>
2023-11-25 20:36:49 +0000

    feat: installed styled-components, created header components, created icons components

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -5,7 +5,7 @@ import Image from 'next/image'
 export default function Home() {
   return (
     <main className="bg-white">
-      Home Page
+      <Header />
     </main>
   )
 }
--- frontend/components/header/index.tsx
+++ frontend/components/header/index.tsx
@@ -1,37 +1,75 @@
 "use client"
-import React, { useEffect, useState } from 'react'
-import { AiDocLogo } from '../icons'
-import Link from 'next/link'
-import { cx } from '@/utils'
+import Link from "next/link"
+import Icons from "../shared/icons"
+import styled from "styled-components"
+
+const StyledHeader = styled.header`
+  header {
+    display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-between;
+  }
+  padding: 39px 80px 39px 80px;
+  font-size: 16px;
+  font-weight: 600;
+  nav {
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    gap: 20px;
+  }
+  div {
+     gap: 20px;
+  }
+  p:nth-of-type(1) {
+    color: var(--Gray-1, #333);
+  }
+  .btn {
+    background-color: #08426B;
+    width: 95px;
+    height: 38px;
+    text-align: center;
+    padding: 7px 24px;
+    border-radius: 118px;
+    font-size: 12px;
+    font-weight: 700;
+  }
+`
 
 const Header = () => {
-  const [isSticky, setSticky] = useState(false);
-
-  useEffect(() => {
-    const handleScroll = () => {
-      setSticky(window.scrollY > 0);
-    };
-
-    window.addEventListener('scroll', handleScroll);
-
-    return () => {
-      window.removeEventListener('scroll', handleScroll);
-    };
-  }, []);
-  return (
-    <header className={cx(isSticky ? 'sticky top-0 py-4' : 'py-10','flex justify-between px-10 py-10 items-center bg-slate-200/80 backdrop-blur-sm')}>
-      <AiDocLogo className='h-16'/>
-
-      <nav className='flex gap-6 absolute right-1/2 translate-x-1/2 text-blueDark-200 font-semibold'>
-        <Link href='/about'>About us</Link>
-        <Link href='/contact'>Contact us</Link>
+  const navLinks = [
+    {
+      text: 'About us',
+      link: '/about'
+    },
+    {
+      text: 'Contact us',
+      link: 'contact'
+    }
+  ]
+  return ( <StyledHeader>
+    <header>
+      <Link href='/'>
+        <Icons type="logo" />    
+      </Link>
+      <nav>
+        {navLinks.map((item, index) => (
+          <Link href={item.link} key={index}>
+            <span>{item.text}</span>
+          </Link>
+        ))}
       </nav>
-
-      <div>
-        Christian
+      <div className="flex-row">
+        <Link href='/login'>
+           <p>Login</p>
+        </Link>
+        <Link href='/signup'>
+           <p className="btn flex-row" style={{color: '#fff'}}>SIGN UP</p>
+        </Link>
       </div>
     </header>
+    </StyledHeader>
   )
 }
-
 export default Header
