d878e669eae35017f94a74dc8298bf4150387f21
nonfungiblehayor <nonfungiblehayor.eth@gmail.com>
2023-11-25 21:13:22 +0000

    feat: How section

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -2,12 +2,14 @@ import Header from '@/components/header'
 import SubHeader from '@/components/SubHeader'
 import { AiDocLogo, BloodIcon, BotIcon, ChevronUpIcon, DnaIcon, LogoutIcon, SpecialistIcon, SunIcon, WeightIcon } from '@/components/icons'
 import Image from 'next/image'
+import How from '@/components/How'
 
 export default function Home() {
   return (
     <main className="bg-white">
       <Header />
       <SubHeader />
+      <How />
     </main>
   )
 }
