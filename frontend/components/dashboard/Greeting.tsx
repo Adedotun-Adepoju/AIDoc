2a614fe0f6cf75be4534c573f04319fdd2f9d9e8
christianonoh <onohchibyk@gmail.com>
2023-11-26 14:15:41 +0100

    Add ChatWithDocBotBtn and ConnectDocBtn components

--- frontend/components/dashboard/Greeting.tsx
+++ frontend/components/dashboard/Greeting.tsx
@@ -2,6 +2,8 @@
 
 import { useEffect, useState } from "react";
 import { BotIcon, SunIcon } from "../icons";
+import ChatWithDocBotBtn from "../elements/ChatWithDocBotBtn";
+import ConnectDocBtn from "../elements/ConnectDocBtn";
 
 const Greeting = ({ ...props }) => {
   const [greeting, setGreeting] = useState("");
@@ -33,23 +35,18 @@ const Greeting = ({ ...props }) => {
         <span>{greeting ? greeting : "Hello!"}, {props.user.name}</span>
         <span>{greeting === "Good morning" ? <SunIcon className="w-10 h-10" /> : greeting === "Good morning" ? <SunIcon className="w-10 h-10" /> : <SunIcon className="w-10 h-10" />}</span>
       </h2>
-      <h2 className="mb-6 text-3xl font-bold xs:text-4xl">How are you feeling today?</h2>
+      <h2>How are you feeling today?</h2>
       <p className="max-w-xl mb-6 text-base font-medium">
         Your well-being is our top priority! To ensure accurate and personalized
         guidance, please share all relevant details about your health with
         AI-DOC. Don't hesitate to provide comprehensive information.
       </p>
       <div className="flex gap-4 font-bold">
-        <button className="flex items-center justify-center w-1/2 gap-2 px-2 py-4 text-sm uppercase rounded-lg sm:gap-4 sm:px-6 sm:text-xl sm:w-3/5 bg-blueLight sm:whitespace-nowrap">
-          <BotIcon className="h-10"/>
-          <span>chat with ai-doc</span>
-        </button>
-        <button className="flex items-center justify-center w-1/2 px-6 text-sm rounded-lg sm:text-base sm:w-2/5 bg-green">
-          <span className="inline-block text-left">Connect with a Health Practitioner</span>
-        </button>
+        <ChatWithDocBotBtn />
+        <ConnectDocBtn />
       </div>
     </div>
   );
-};
+}
 
 export default Greeting;
