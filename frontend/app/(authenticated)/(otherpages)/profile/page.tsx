1596e3b1c66b0f4e49d99e8a7435351720ee8c9a
christianonoh <onohchibyk@gmail.com>
2023-11-27 00:41:05 +0100

    Refactor profile page layout and add back button

--- frontend/app/(authenticated)/(otherpages)/profile/page.tsx
+++ frontend/app/(authenticated)/(otherpages)/profile/page.tsx
@@ -12,26 +12,23 @@ import profileImg from "@/public/images/profile.png";
 import ConnectDocBtn from "@/components/elements/ConnectDocBtn";
 import ChatWithDocBotBtn from "@/components/elements/ChatWithDocBotBtn";
 import Button from "@/components/elements/Button";
-import Link from "next/link";
+import BackBtn from "@/components/elements/BackBtn";
 
 const ProfilePage = () => {
   return (
     <main className="px-5 mx-auto max-w-7xl md:px-10 relative">
-      <Link href='/' className="sticky top-24 left-0 flex w-max items-center rounded-xl bg-blueLight/10 gap-2 text-base font-semibold text-blueDark-200 py-2 px-3  hover:bg-blueLight/20 transition-all duration-200 ease-in-out hover:scale-105">
-        <BackIcon className="w-5 h-5" />
-        Back
-      </Link>
-      <section className="grid max-w-5xl grid-cols-12 mx-auto mt-8">
-        
-        <div className="flex flex-col justify-end col-span-9">
-          <h2 className="w-full mb-12 text-2xl font-semibold capitalize self-start">
-            My profile
-          </h2>
-          <div className="flex gap-8 mb-16">
+      <BackBtn />
+      <section className="max-w-5xl mx-auto mt-8">
+        <h2 className="w-full mb-12 text-2xl font-semibold capitalize lg:absolute lg:top-20">
+          My profile
+        </h2>
+        <div className="flex flex-col md:flex-row">
+        <div className="flex flex-col flex-1 justify-end relative">
+          <div className="flex lg:flex-row flex-col gap-8 mb-16">
             <Image
               src={profileImg}
               alt="profile"
-              className="w-64 h-64 border rounded-lg"
+              className="w-full md:w-72 md:h-72 border rounded-lg"
             />
             <div className="flex flex-col justify-end">
               <h2 className="mb-6 text-3xl font-bold xs:text-4xl">
@@ -57,15 +54,15 @@ const ProfilePage = () => {
               </div>
             </div>
           </div>
-          <div className="flex max-w-lg gap-4 font-bold text-white">
-            <ChatWithDocBotBtn />
-            <ConnectDocBtn />
+          <div className="flex max-w-lg gap-4 font-bold text-white mr-4">
+            <ChatWithDocBotBtn classNames="md:w-7/12 lg:w-3/5"/>
+            <ConnectDocBtn classNames="md:w-5/12 lg:w-2/5" />
           </div>
         </div>
 
         {/* STATUS  */}
-        <div className="col-span-3 px-8 py-6 rounded-lg bg-blueDark-100">
-          <div className="grid grid-cols-1 grid-rows-3 gap-2 lg:gap-2 sm:gap-4">
+        <div className="sm:w-4/12 ">
+          <div className="grid grid-cols-1 grid-rows-3 gap-2 lg:gap-2 sm:gap-4 bg-blueDark-100 min-w-[208px] px-8 py-6 rounded-lg ">
             <div className="col-span-1 p-4 bg-white rounded-lg">
               <p className="mb-3 font-semibold text-black">Weight (KG)</p>
               <span className="flex items-center justify-between text-5xl font-extrabold sm:text-6xl text-blueLight">
@@ -89,8 +86,24 @@ const ProfilePage = () => {
                 <BloodIcon className="h-12" />
               </span>
             </div>
-            <Button title='Update Data' classNames="bg-blueLight mt-6 text-white sm:px-4 sm:py-2 w-10/12" />
+            <div className="gradient_border">
+              <div className="flex flex-col justify-around h-full col-span-1 p-4 rounded-lg bg-blueDark-100">
+                <p className="mb-3 text-base font-semibold text-white capitalize sm:text-lg">
+                  Last Chat with AI-DOC
+                </p>
+                <span className="flex items-center justify-between text-lg font-bold text-white sm:text-3xl lg:text-xl xl:text-3xl">
+                  4days ago
+                </span>
+              </div>
+            </div>
+            <div>
+              <Button
+              title="Update Data"
+              classNames="bg-blueLight mt-6 text-white sm:px-2 sm:py-2 w-10/12"
+            />
+            </div>
           </div>
+        </div>
         </div>
       </section>
     </main>
