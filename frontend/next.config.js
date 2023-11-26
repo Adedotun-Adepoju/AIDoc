a65fc69bcacf6b302e612ae49df20f9ed7bb4260
christianonoh <onohchibyk@gmail.com>
2023-11-26 14:17:36 +0100

    Add rewrites for chat API endpoints

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,4 +1,17 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  async rewrites() {
+    return [
+      {
+        source: "/api/chat/conversation",
+        destination: "https://aidoc.up.railway.app/chat/conversation",
+      },
+      {
+        source: "/api/chat/prompt",
+        destination: "https://aidoc.up.railway.app/chat/prompt",
+      },
+    ];
+  },
+}
 
 module.exports = nextConfig
