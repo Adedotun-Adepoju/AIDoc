4815fbdf85c98faa7fc9dc793c4ef0cc72867279
Christian Onoh <chibyk5000@gmail.com>
2023-11-27 11:05:34 +0100

    Update ChatWithDocBotBtn styles

--- frontend/components/elements/ChatWithDocBotBtn.tsx
+++ frontend/components/elements/ChatWithDocBotBtn.tsx
@@ -8,8 +8,8 @@ interface ChatWithDocBotBtnProps {
 
 const ChatWithDocBotBtn = ({...props}: ChatWithDocBotBtnProps) => {
   return (
-    <Link href='/chatbox' className={cx("flex items-center justify-center w-1/2 gap-2 px-2 py-4 text-sm uppercase rounded-lg sm:gap-4 sm:px-6 sm:text-xl sm:w-3/5 bg-blueLight sm:whitespace-nowrap", props.classNames)}>
-      <BotIcon className="h-10"/>
+    <Link href='/chatbox' className={cx("flex items-center justify-center w-1/2 gap-1 px-2 py-4 text-sm uppercase rounded-lg sm:gap-4 sm:px-6 sm:text-xl sm:w-3/5 bg-blueLight sm:whitespace-nowrap", props.classNames)}>
+      <BotIcon className="h-4 sm:h-10"/>
       <span>chat with ai-doc</span>
     </Link>
   )
