30016c190d20776274ea40c9aa087c1fb52e8aec
Christian Onoh <chibyk5000@gmail.com>
2023-11-27 11:01:16 +0100

    Add z-index to BackBtn

--- frontend/components/elements/BackBtn.tsx
+++ frontend/components/elements/BackBtn.tsx
@@ -5,7 +5,7 @@ const BackBtn = () => {
   return (
     <Link
       href="/"
-      className="sticky top-24 left-0 flex w-max items-center rounded-xl bg-white gap-2 text-base font-semibold text-blueDark-200 py-2 px-3  hover:bg-blueLight/20 transition-all duration-200 ease-in-out hover:scale-105"
+      className="sticky z-20 top-24 left-0 flex w-max items-center rounded-xl bg-white gap-2 text-base font-semibold text-blueDark-200 py-2 px-3  hover:bg-blueLight/20 transition-all duration-200 ease-in-out hover:scale-105"
     >
       <BackIcon className="w-5 h-5" />
       Back
