16a402f4d813b3a3363970cc5f6c574f9eddfe1c
Christian Onoh <chibyk5000@gmail.com>
2023-11-27 05:27:13 +0100

    Refactor Button component to use Next.js Link

--- frontend/components/elements/Button.tsx
+++ frontend/components/elements/Button.tsx
@@ -1,16 +1,18 @@
 import { cx } from '@/utils'
+import Link from 'next/link'
 import React from 'react'
 
 interface ButtonProps {
   title: string,
-  classNames: string,
+  classNames?: string,
+  href: string
 }
 
 const Button = ({ ...props }: ButtonProps ) => {
   return (
-    <button className={cx('px-4 py-2 text-sm font-semibold uppercase rounded-full sm:py-4 sm:px-6 bg-blueDark-200', props.classNames ? props.classNames : '')}>
+    <Link href={props.href} className={cx('px-4 py-2 text-sm font-semibold uppercase rounded-full sm:py-4 sm:px-6 bg-blueDark-200 ', props.classNames)}>
       {props.title}
-    </button>
+    </Link>
   )
 }
 
