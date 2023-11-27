45de449cbff2a9c823179700561eeed7d926a930
Christian Onoh <chibyk5000@gmail.com>
2023-11-27 11:05:51 +0100

    Update ConnectDocBtn styling

--- frontend/components/elements/ConnectDocBtn.tsx
+++ frontend/components/elements/ConnectDocBtn.tsx
@@ -7,7 +7,7 @@ interface ConnectDocBtnProps {
 
 const ConnectDocBtn = ({...props}: ConnectDocBtnProps) => {
   return (
-    <Link href='/connect-specialist' className={cx("flex items-center justify-center w-1/2 px-6 text-sm rounded-lg sm:text-base sm:w-2/5 bg-green", props.classNames)}>
+    <Link href='/connect-specialist' className={cx("flex items-center justify-center w-1/2 px-4 py-1 text-sm rounded-lg sm:text-base sm:w-2/5 bg-green", props.classNames)}>
       <span className="inline-block text-left">Connect with a Health Practitioner</span>
     </Link>
   )
