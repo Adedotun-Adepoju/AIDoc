f4ca598bd6b34df3affc6e85687d26beccf62933
Christian Onoh <chibyk5000@gmail.com>
2023-11-28 12:21:40 +0100

    Update UserMessage component to include user_name prop

--- frontend/components/chatbox/UserMessage.tsx
+++ frontend/components/chatbox/UserMessage.tsx
@@ -5,6 +5,7 @@ import profileImg from "@/public/images/profile.png";
 interface UserMessageProps {
   content: string;
   speed?: number;
+  user_name: string;
 }
 
 const user = {
@@ -12,12 +13,12 @@ const user = {
   avatar: 'https://i.pravatar.cc/150?img=68',
 };
 
-const UserMessage: React.FC<UserMessageProps> = ({ content }) => {
+const UserMessage: React.FC<UserMessageProps> = ({ content, user_name }) => {
   return (
     <div className={cx('flex items-center w-full py-2')}>
       <div><Image src={profileImg} alt={user.name} width={42} height={42} className={cx('rounded-full mr-2')} /></div>
       <div className='text-black p-3 rounded-lg flex flex-col'>
-        <span className='text-blueLight text-base font-semibold'>{user.name}</span>
+        <span className='text-blueLight text-base font-semibold'>{user_name}</span>
         <span className="text-black text-base leading-tight font-medium">
         {content}
         </span>
