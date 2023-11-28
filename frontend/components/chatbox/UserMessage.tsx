1dcdf0c8ed9f46ae1afc3dd7fc3f505a4f212d5a
Christian Onoh <chibyk5000@gmail.com>
2023-11-28 12:22:03 +0100

    Update user profile image alt attribute

--- frontend/components/chatbox/UserMessage.tsx
+++ frontend/components/chatbox/UserMessage.tsx
@@ -8,15 +8,10 @@ interface UserMessageProps {
   user_name: string;
 }
 
-const user = {
-  name: 'John Doe',
-  avatar: 'https://i.pravatar.cc/150?img=68',
-};
-
 const UserMessage: React.FC<UserMessageProps> = ({ content, user_name }) => {
   return (
     <div className={cx('flex items-center w-full py-2')}>
-      <div><Image src={profileImg} alt={user.name} width={42} height={42} className={cx('rounded-full mr-2')} /></div>
+      <div><Image src={profileImg} alt={user_name} width={42} height={42} className={cx('rounded-full mr-2')} /></div>
       <div className='text-black p-3 rounded-lg flex flex-col'>
         <span className='text-blueLight text-base font-semibold'>{user_name}</span>
         <span className="text-black text-base leading-tight font-medium">
