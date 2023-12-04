518850f7b72eb245baae1fb9d3dc270897032336
Christian Onoh <chibyk5000@gmail.com>
2023-12-04 16:07:25 +0100

    Remove unused imports and console.log statements

--- frontend/app/(authenticated)/(chatbox)/chatbox/page.tsx
+++ frontend/app/(authenticated)/(chatbox)/chatbox/page.tsx
@@ -1,20 +1,17 @@
 "use client";
 
-import AiDocMessage from "@/components/chatbox/AiDocMessage";
 import ChatHistory from "@/components/chatbox/ChatHistory";
 import SideBar from "@/components/chatbox/SideBar";
 import { SendIcon } from "@/components/icons";
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import {
   bearerToken,
   checkLoggedIn,
-  cx,
   initialConvoState,
   initialConvoStatement,
   queryGPT,
   saveConvo,
   savePrompt,
-  systemPrompt,
 } from "@/utils";
 import { Twirl as Hamburger } from "hamburger-react";
 import { useRouter } from "next/navigation";
@@ -114,7 +111,6 @@ const ChatBoxPage = () => {
 
   const startConversation = async () => {
     try {
-      // console.log(`Starting conversation...userInput = ${userInput}`);
       if (conversation.chatMessages.length === 1) {
         conversation.chatMessages.push({ role: "user", content: userInput });
       }
@@ -221,55 +217,6 @@ const ChatBoxPage = () => {
           </h2>
 
           {/* Chat messages */}
-          {/* <div className="flex-1 px-5 overflow-y-auto border_b_except_last_child">
-            <AiDocMessage
-              setDivHeight={setDivHeight}
-              content={`Hello ${user_data?.first_name}, what symptoms are you having today?`}
-              typing={true}
-            />
-            {conversation.chatMessages.slice(1).map((message, index) => {
-              return message.role === "assistant" ? (
-                <AiDocMessage
-                  key={index}
-                  content={message.content}
-                  setDivHeight={setDivHeight}
-                  typing={conversation.typing}
-                />
-              ) : (
-                <UserMessage
-                  key={index}
-                  content={message.content}
-                  user_name={user_data?.first_name}
-                />
-              );
-            })}
-            {conversation.chatMessages[conversation.chatMessages.length - 1]
-              .role === "user" && conversation.typing ? (
-              <div className={cx("flex items-start w-full py-2")}>
-                <div className="py-3 ">
-                  <AiDocLogo
-                    className={cx(
-                      "w-8 h-8 mr-2 rounded-full border border-blueDark-200"
-                    )}
-                  />
-                </div>
-                <div className="flex flex-col p-3 rounded-lg">
-                  <span className="text-base font-semibold text-blueDark-200">
-                    AiDoc
-                  </span>
-                  <span
-                    className={cx(
-                      "text-black text-base leading-tight font-medium",
-                      "whitespace-pre-wrap"
-                    )}
-                  >
-                    <TypingLoadingIcon className="w-5 h-5 mr-2" />
-                  </span>
-                </div>
-              </div>
-            ) : null}
-            <div ref={ref} />
-          </div> */}
           <ConversationUI conversation={conversation} user_data={user_data} />
 
           {/* User input area */}
