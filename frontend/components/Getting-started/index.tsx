3314e5a5aa0a983851c43d84591e5cac5026150f
Christian Onoh <chibyk5000@gmail.com>
2023-12-19 10:22:28 +0100

    Refactor GettingStarted and SubHeader components

--- frontend/components/Getting-started/index.tsx
+++ frontend/components/Getting-started/index.tsx
@@ -1,78 +1,87 @@
-"use client"
-import styled from "styled-components"
-import Link from "next/link"
-import AiBot from '../../public/img/ai-bot.png'
-import Image from "next/image"
+"use client";
+import styled from "styled-components";
+import Link from "next/link";
+import AiBot from "../../public/img/ai-bot.png";
+import Image from "next/image";
 const StyledStarted = styled.section`
-    background: var(--Blue-1, #08426B);
-    padding-top: 90px;
+  background: var(--Blue-1, #08426b);
+  padding-top: 30px;
+  .started {
+    justify-content: center;
+  }
+  .content-div {
+    display: flex;
+    flex-direction: column;
+    align-items: flex-start;
+    gap: 15px;
+  }
+  h1 {
+    color: #fff;
+    font-size: 39px;
+    font-weight: 700;
+    width: 383px;
+    line-height: normal;
+  }
+  p {
+    color: #fff;
+    font-size: 14px;
+    font-weight: 600;
+    width: 455px;
+    line-height: 16.8px;
+  }
+  button {
+    width: 180px;
+    height: 44px;
+    border-radius: 118px;
+    background: #1ca1fe;
+    color: #fff;
+    font-size: 12px;
+    font-weight: 700;
+  }
+  @media (max-width: 768px) {
     .started {
-        justify-content: center;
-    }
-    .content-div {
-        display: flex;
-        flex-direction: column;
-        align-items: flex-start;
-        gap: 15px;
+      display: flex;
+      flex-direction: column;
+      padding-left: 15px;
     }
     h1 {
-        color: #fff;
-        font-size: 39px;
-        font-weight: 700;
-        width: 383px;
+      font-size: 25px;
+      width: 90vw;
     }
     p {
-        color: #fff;
-        font-size: 14px;
-        font-weight: 600;
-        width: 455px;
-        line-height: 16.8px;
+      width: 85vw;
+      font-size: 14px;
     }
-    button {
-        width: 180px;
-        height: 44px;
-        border-radius: 118px;
-        background: #1CA1FE;
-        color: #fff;
-        font-size: 12px;
-        font-weight: 700;
+    .ai-banner {
+      width: 90vw;
     }
-    @media (max-width: 768px) {
-        .started {
-            display: flex;
-            flex-direction: column;
-            padding-left: 15px;
-        }
-        h1 {
-            font-size: 25px;
-            width: 90vw;
-        }
-        p {
-            width: 85vw;
-            font-size: 14px;
-        }
-        .ai-banner {
-            width: 90vw;
-        }
-    }
-`
+  }
+`;
 const GettingStarted = () => {
-    return ( <StyledStarted>
-        <section className="flex__row started">
-            <div className="content-div">
-            <h1> Getting Started with AI-DOC</h1>
-                <p>
-                    Making informed decisions about your health has never been easier. Join AI-DOC today and take control of your well-being.
-                </p>
-               <Link href='/signup'>
-                    <button>GET STARTED</button>
-                </Link>
-            </div>
-            <div className="img-div">
-                <Image src={AiBot} alt="ai banner" className="ai-banner" width={391} height={387} />
-            </div>
-        </section>
-        </StyledStarted>
-    )
-}
-export default GettingStarted
+  return (
+    <StyledStarted>
+      <section className="flex__row started">
+        <div className="content-div">
+          <h1> Getting Started with AI-DOC</h1>
+          <p>
+            Making informed decisions about your health has never been easier.
+            Join AI-DOC today and take control of your well-being.
+          </p>
+          <Link href="/signup">
+            <button>GET STARTED</button>
+          </Link>
+        </div>
+        <div className="img-div">
+          <Image
+            src={AiBot}
+            alt="ai banner"
+            className="ai-banner"
+            width={391}
+            height={387}
+          />
+        </div>
+      </section>
+    </StyledStarted>
+  );
+};
+export default GettingStarted;
--- frontend/components/SubHeader/index.tsx
+++ frontend/components/SubHeader/index.tsx
@@ -1,87 +1,100 @@
-"use client"
-import styled from "styled-components"
-import AI from '../../public/img/Ai-banner.png'
-import Image from "next/image"
-import Link from "next/link"
+"use client";
+import styled from "styled-components";
+import AI from "../../public/img/Ai-banner.png";
+import Image from "next/image";
+import Link from "next/link";
 
 const StyledSubHeader = styled.section`
-    gap: 22px;
-    margin-top: 127px;
+  gap: 22px;
+  margin-top: 75px;
+  .subhead {
+    justify-content: center;
+  }
+  .content-div {
+    display: flex;
+    flex-direction: column;
+    align-items: flex-start;
+    gap: 15px;
+  }
+  h1,
+  h2 {
+    color: var(--Blue-1, #08426b);
+    font-size: 39px;
+    font-weight: 700;
+    line-height: normal;
+  }
+  h2 {
+    color: var(--Blue-2, #1ca1fe);
+    width: 510px;
+  }
+  p {
+    color: var(--Black, #101010);
+    font-size: 14px;
+    font-weight: 600;
+    width: 455px;
+    line-height: 16.8px;
+  }
+  button {
+    width: 180px;
+    height: 44px;
+    border-radius: 118px;
+    background: var(--Blue-1, #08426b);
+    color: #fff;
+    font-size: 12px;
+    font-weight: 700;
+  }
+  @media (max-width: 768px) {
     .subhead {
-        justify-content: center;
+      display: flex;
+      flex-direction: column;
     }
-    .content-div {
-        display: flex;
-        flex-direction: column;
-        align-items: flex-start;
-        gap: 15px;
-    }
-    h1,h2 {
-        color: var(--Blue-1, #08426B);
-        font-size: 39px;
-        font-weight: 700;
-    }
-    h2 {
-        color: var(--Blue-2, #1CA1FE);
-        width: 510px;
+    h1 {
+      font-size: 25px;
     }
     p {
-        color: var(--Black, #101010);
-        font-size: 14px;
-        font-weight: 600;
-        width: 455px;
-        line-height: 16.8px;
+      font-size: 13px;
+      width: 90vw;
     }
-    button {
-        width: 180px;
-        height: 44px;
-        border-radius: 118px;
-        background: var(--Blue-1, #08426B);
-        color: #fff;
-        font-size: 12px;
-        font-weight: 700;
+    h2 {
+      font-size: 31px;
+      width: 90vw;
     }
-    @media (max-width: 768px) {
-        .subhead {
-            display: flex;
-            flex-direction: column;
-        }
-        h1 {
-            font-size: 25px;
-        }
-        p {
-            font-size: 13px;
-            width: 90vw;
-        }
-        h2 {
-            font-size: 31px;
-            width: 90vw;
-        }
-        .img-bot {
-            width: 276px;
-            height: 274px;
-            margin-top: 48px;
-        }
+    .img-bot {
+      width: 276px;
+      height: 274px;
+      margin-top: 48px;
     }
-`
+  }
+`;
 const SubHeader = () => {
-    return ( <StyledSubHeader>
-        <section className="flex__row subhead">
-            <div className="content-div">
-                <h1>Welcome to AI-DOC:</h1>
-                <h2>
-                    Your Intelligent Health Care Companion
-                </h2>
-                <p>
-                    AI-DOC is an innovative web application that harnesses the power of artificial intelligence to provide personalized symptom analysis and preliminary medical advice. Our mission is to empower users to make informed decisions about their health, offering guidance that is quick, reliable, and accessible.
-                </p>
-                <Link href='/signup'><button>GET STARTED</button></Link>
-            </div>
-            <div className="img-div">
-                <Image src={AI} alt="ai banner" width={391} height={387} className="img-bot"/>
-            </div>
-        </section>
-        </StyledSubHeader>
-    )
-}
-export default SubHeader
+  return (
+    <StyledSubHeader>
+      <section className="flex__row subhead">
+        <div className="content-div">
+          <h1>Welcome to AI-DOC:</h1>
+          <h2>Your Intelligent Health Care Companion</h2>
+          <p>
+            AI-DOC is an innovative web application that harnesses the power of
+            artificial intelligence to provide personalized symptom analysis and
+            preliminary medical advice. Our mission is to empower users to make
+            informed decisions about their health, offering guidance that is
+            quick, reliable, and accessible.
+          </p>
+          <Link href="/signup">
+            <button>GET STARTED</button>
+          </Link>
+        </div>
+        <div className="img-div">
+          <Image
+            src={AI}
+            alt="ai banner"
+            width={391}
+            height={387}
+            className="img-bot"
+          />
+        </div>
+      </section>
+    </StyledSubHeader>
+  );
+};
+export default SubHeader;
